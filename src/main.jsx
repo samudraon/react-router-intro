cefa336e83710d0267d1f1c016d740e1c6403adb
samudraon <samudraon@gmailcom>
2023-04-03 11:34:57 +0600

    avoid reload using Link and load data

---
+++ src/main.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Error from './components/Error/Error';
+import First from './components/First/First';
+import Friends from './components/Friends/Friends';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App></App>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact>
+//   }
+// ])
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <First></First>
+      },
+      {
+        path: 'friends',
+        element: <Friends></Friends>,
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      },
+    ]
+  },
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
